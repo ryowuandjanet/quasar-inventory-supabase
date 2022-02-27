4cfa3c9939e7dd174b114520cfb217bad2023b21
ryowuandjanet <ryowu0329@gmail.com>
2022-02-27 12:17:44 +0800

    #3

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,7 +47,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
@@ -68,10 +68,12 @@ module.exports = configure(function (ctx) {
 
       // https://quasar.dev/quasar-cli/handling-webpack
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
+      /*
       chainWebpack (chain) {
         chain.plugin('eslint-webpack-plugin')
           .use(ESLintPlugin, [{ extensions: ['js', 'vue'] }])
       }
+      */
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
--- src/boot/supabase.js
+++ src/boot/supabase.js
@@ -1,10 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
+import useAuthUser from 'src/composables/UseAuthUser'
 
 const supabaseUrl = 'https://wyiaqsgvsxmtvunehsoq.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5aWFxc2d2c3htdHZ1bmVoc29xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU5MzA0MTMsImV4cCI6MTk2MTUwNjQxM30.VmwJ5DhYmPGJuF7cM0uU1MyX4vw9Y914nPpS9tRmOLY'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-console.log('init supabase:', supabase)
+supabase.auth.onAuthStateChange((event, session) => {
+  const { user } = useAuthUser()
+
+  user.value = session?.user || null
+})
 
 export default function useSupabase () {
   return { supabase }
