104a8d82a6481d177225e485d1000d1f199fddf8
ryowuandjanet <ryowu0329@gmail.com>
2022-02-27 12:42:31 +0800

    #4

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,8 @@
 import { route } from 'quasar/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
+import useAuthUser from 'src/composables/UseAuthUser'
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Router instance.
- */
 
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
@@ -19,11 +12,19 @@ export default route(function (/* { store, ssrContext } */) {
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
-
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE)
+  })
+
+  Router.beforeEach((to) => {
+    const { isLoggedIn } = useAuthUser()
+
+    if (
+      !isLoggedIn() &&
+      to.meta.requiresAuth &&
+      !Object.keys(to.query).includes('fromEmail')
+    ) {
+      return { name: 'login' }
+    }
   })
 
   return Router
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,16 +7,19 @@ const routes = [
       { path: '', name: 'loginDefault', component: () => import('pages/Login.vue') },
       { path: 'login', name: 'login', component: () => import('pages/Login.vue') },
       { path: 'register', name: 'register', component: () => import('pages/Register.vue') },
-      { path: 'email-confirmation', name: 'email-confirmation', component: () => import('pages/EmailConfirmation.vue') },
-      { path: 'me', name: 'me', component: () => import('pages/Me.vue') }
+      { path: 'email-confirmation', name: 'email-confirmation', component: () => import('pages/EmailConfirmation.vue') }
     ]
   },
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
+      { path: 'me', name: 'me', component: () => import('pages/Me.vue') }
+      //{ path: '', component: () => import('pages/Index.vue') }
+    ],
+    meta: {
+      requiresAuth: true
+    }
   },
 
   // Always leave this as last one,
